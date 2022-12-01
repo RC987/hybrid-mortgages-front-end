72bc8276094a22b0b070511cda3157782602e001
Samantha Wu <samanthayywu@gmail.com>
2022-12-01 14:40:52 +0000

    form

--- hybrid-mortgages/src/components/Form.jsx
+++ hybrid-mortgages/src/components/Form.jsx
@@ -1,8 +1,40 @@
-export default Form(){
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 
-    return (
-        <>
-        
-        </>
-    )
+export default function Form() {
+  return (
+    <>
+      <FormControl isRequired>
+        <FormLabel>Full Name</FormLabel>
+        <Input placeholder="Full name" />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Sort Code</FormLabel>
+        <Input placeholder="Sort Code" />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Account Number</FormLabel>
+        <Input placeholder="Account Number" />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Date of Birth</FormLabel>
+        <Input placeholder="DoB" />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Address</FormLabel>
+        <Input placeholder="Address" />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Annual Salary</FormLabel>
+        <Input placeholder="Salary" />
+      </FormControl>
+      <Button>Save</Button>
+    </>
+  );
 }
