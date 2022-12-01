b4a447f71ee5a1b3e1243aa6d60f5090c1cb50e7
RC987 <44164304+RC987@users.noreply.github.com>
2022-12-01 13:33:21 +0000

    Deploying in Static Web Apps

--- hybrid-mortgages/src/main.jsx
+++ hybrid-mortgages/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import * as ReactDOM from "react-dom/client";
 import { extendTheme } from "@chakra-ui/react";
 
 const colors = {
