9cd67ce7e2c75991a6b43bff1ed4958bc18b3db5
Samantha Wu <samanthayywu@gmail.com>
2022-12-01 15:09:28 +0000

    newMortgage

--- hybrid-mortgages/src/App.jsx
+++ hybrid-mortgages/src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import LandingPage from "./components/LandingPage";
 import Mortgage from "./components/Mortgage";
 import Form from "./components/Form";
+import NewMortgage from "./components/NewMortgage";
 
 function App() {
   return (
     <>
       {/* <LandingPage /> */}
-      <Mortgage />
+      {/* <Mortgage /> */}
       {/* <Form /> */}
+      <NewMortgage />
     </>
   );
 }
---
+++ hybrid-mortgages/src/components/NewMortgage.jsx
@@ -0,0 +1,72 @@
+import { Container, Box, VStack, Button } from "@chakra-ui/react";
+import {
+  Editable,
+  EditableInput,
+  EditableTextarea,
+  EditablePreview,
+} from "@chakra-ui/react";
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from "@chakra-ui/react";
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+export default function Mortgage() {
+  const format = (val) => `£` + val;
+  const parse = (val) => val.replace(/^\£/, "");
+
+  const [value, setValue] = useState("500");
+  return (
+    <Container>
+      <p>Monthly Payment</p>
+      <NumberInput
+        onChange={(valueString) => setValue(parse(valueString))}
+        value={format(value)}
+        max={5000}
+      >
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+      <Box maxW="lg" borderWidth="10px" borderRadius="lg" overflow="hidden">
+        <Stat>
+          <StatLabel>Mortgage Term</StatLabel>
+          <StatNumber>25 years</StatNumber>
+        </Stat>
+      </Box>
+      <Box maxW="lg" borderWidth="10px" borderRadius="lg" overflow="hidden">
+        <Stat>
+          <StatLabel>Current Interest Rate</StatLabel>
+          <StatNumber>3%</StatNumber>
+          <StatHelpText>01 Dec</StatHelpText>
+        </Stat>
+      </Box>
+      <Box>
+        <Stat>
+          <StatLabel>Remaining Balance</StatLabel>
+          <StatNumber>£275,000.00</StatNumber>
+        </Stat>
+      </Box>
+      <Box>
+        <Stat>
+          <StatLabel>Total Balance</StatLabel>
+          <StatNumber>£275,000.00</StatNumber>
+        </Stat>
+      </Box>
+      <Button>Update</Button>
+    </Container>
+  );
+}
